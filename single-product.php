f5bb387d26c1360b651e2937eca9710d49ebe8fa
JuanTamarit <116755072+JuanTamarit@users.noreply.github.com>
2023-04-12 17:38:31 +0200

    Página de producto
    Página de producto mejorada

--- single-product.php
+++ single-product.php
@@ -1,39 +1,69 @@
-<?php get_header(); ?>
+<?php get_header(); the_post(); ?>
 
+    <!-- CONTENIDO ÚNICO DE LA PÁGINA DE PRODUCTO -->
+    <main class="product-page-content">
+        <p class="breadcrumbs">Tienda > <?php the_title(); ?></p>
+        <h1><?php the_title(); ?></h1>
+        <div class="product-component">
+            <div>
+                <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' ); ?>" alt="Producto">
+            </div>
 
-    <?php
+            <div>
+                <div class="product-component__basic-data">
+                    <h1 class="h5"><?php
+                        $product_id = get_the_ID();
+                        $product_price = get_post_meta( $product_id, '_regular_price', true );
+                        echo $product_price;
+                        ?>€
+                    </h1>
+                </div>
 
-		if ( ! defined( 'ABSPATH' ) ) {
-			exit; // Exit if accessed directly
-		}
+                <p class="product-component__description"><?php
+                    $product_id = get_the_ID();
+                    $product_description = get_post_field( 'post_content', $product_id );
+                    echo $product_description;
+                    ?>
+                </p>              
+                                                
+                <div class="product-component__selectors">
+                    <select class="product-component__selector" name="talla">
+                        <option value="TALLA">TALLA</option>
+                        <option value="S">S</option>
+                        <option value="M" selected>M</option>
+                        <option value="L">L</option>
+                    </select> 
+                    
+                    <select class="product-component__selector" name="cantidad">
+                        <option value="CANTIDAD">CANTIDAD</option>
+                        <option value="1">1</option>
+                        <option value="2" selected>2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
 
-    get_header( 'shop' ); ?>
+                    <?php
+                    function imprimir_boton_anadir_al_carrito() {
+                        global $product;
+                        woocommerce_template_loop_add_to_cart( array(
+                            'quantity'   => 1,  // Cantidad predeterminada es 1.
+                            'class'      => 'btn', // Clase CSS del botón.
+                            'attributes' => array(
+                                'data-product_id'  => $product->get_id(), // ID del producto.
+                                'data-product_sku' => $product->get_sku(), // SKU del producto.
+                                'aria-label'       => $product->add_to_cart_description(), // Descripción para accesibilidad.
+                                'rel'              => 'nofollow', // Atributo rel del enlace.
+                            ),
+                        ) );
+                    }
+                    imprimir_boton_anadir_al_carrito();
+                    ?>
 
-	<?php
-		/*
-		 * woocommerce_before_main_content hook.
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
+                </div>
 
-	<?php while ( have_posts() ) : ?>
-
-		<?php the_post(); ?>
-
-		<?php wc_get_template_part( 'content', 'single-product' ); ?>
-
-	<?php endwhile; ?>
-
-	<?php
-		/*
-		 * woocommerce_after_main_content hook.
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	?>
-
-
-<?php get_footer(); 
-/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
+            </div>
+        </div>
+    </main>
+    
+<?php get_footer(); ?>
